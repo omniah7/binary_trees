16987984cf60c9b6bc12af9c295ecaa107288e7e
omniah7 <om267165@gmail.com>
2023-08-02 17:37:34 +0300

    add height function to 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,3 +16,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 		r += binary_tree_height(tree->right) + 1;
 	return (l - r);
 }
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height
+ * Return: the height of a binary tree. If tree is NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	binary_tree_t *l, *r;
+	size_t l_size = 0, r_size = 0;
+
+	if (!tree)
+		return (0);
+
+	l = tree->left;
+	r = tree->right;
+
+	if (l)
+		l_size += 1 + binary_tree_height(l);
+	if (r)
+		r_size += 1 + binary_tree_height(r);
+
+	return ((r_size > l_size) ? r_size : l_size);
+}
